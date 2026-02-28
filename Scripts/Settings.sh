3226b5a71946faeec97412a534d98f6ef0b1c3ba
yjzmh <yjzmh@163.com>
2026-03-01 06:55:52 +0800

    Enable NSS firmware version 11.4
    Enable CONFIG_NSS_FIRMWARE_VERSION_11_4 and comment out conditional NSS version settings.

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -54,12 +54,12 @@ if [[ "${WRT_TARGET^^}" == *"QUALCOMMAX"* ]]; then
 	echo "CONFIG_PACKAGE_luci-app-sqm=y" >> ./.config
 	echo "CONFIG_PACKAGE_sqm-scripts-nss=y" >> ./.config
 	#设置NSS版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
-	if [[ "${WRT_CONFIG,,}" == *"ipq50"* ]]; then
-		echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
-	else
-		echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
-	fi
+	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=y" >> ./.config
+	#if [[ "${WRT_CONFIG,,}" == *"ipq50"* ]]; then
+	#	echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
+	#else
+	#	echo "CONFIG_NSS_FIRMWARE_VERSION_12_5=y" >> ./.config
+	#fi
 	#无WIFI配置调整Q6大小
 	if [[ "${WRT_CONFIG,,}" == *"wifi"* && "${WRT_CONFIG,,}" == *"no"* ]]; then
 		find $DTS_PATH -type f ! -iname '*nowifi*' -exec sed -i 's/ipq\(6018\|8074\).dtsi/ipq\1-nowifi.dtsi/g' {} +
